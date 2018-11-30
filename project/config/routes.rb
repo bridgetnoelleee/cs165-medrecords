892473e7ef245cc5719d0d07f065a1982022f0f6
bridgetnoelleee <bridgetlegaspi@gmail.com>
2018-12-01 04:08:04 +0800

    Lab test results successful

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  resources :runtests
+  resources :patienttests
+  resources :patientinfos
+  resources :patienthistories
+  resources :nurses
+  resources :medicalhistories
+  resources :labtestsresults
+  resources :getinfos
+  resources :doctors
+  resources :currentmedicals
+  resources :analyzeordiagnoses
   get 'dashboard/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html'
 
   root 'dashboard#index'
+  # root 'labtestsresults#index'
 end
--- project/db/schema.rb
+++ project/db/schema.rb
@@ -12,13 +12,7 @@
 
 ActiveRecord::Schema.define(version: 0) do
 
-  create_table "analyzeordiagnose", primary_key: ["doctorID", "medHisID"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.integer "doctorID", null: false
-    t.integer "medHisID", null: false
-    t.index ["medHisID"], name: "medHisID"
-  end
-
-  create_table "currentmedical", primary_key: "currMedID", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "CurrentMedical", primary_key: "currMedID", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.timestamp "dateCreated"
     t.timestamp "dateUpdated"
     t.text "medications"
@@ -29,7 +23,7 @@ ActiveRecord::Schema.define(version: 0) do
     t.float "weight"
   end
 
-  create_table "doctor", primary_key: "doctorID", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "Doctor", primary_key: "doctorID", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "docName", limit: 50
     t.string "specialization", limit: 50
     t.string "department", limit: 50
@@ -37,38 +31,25 @@ ActiveRecord::Schema.define(version: 0) do
     t.string "schedule", limit: 50
   end
 
-  create_table "getinfo", primary_key: ["patientID", "nurseID"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.integer "patientID", null: false
-    t.integer "nurseID", null: false
-    t.index ["nurseID"], name: "nurseID"
-  end
-
-  create_table "labtestsresults", primary_key: ["testID", "dateAndTime"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "LabTestsResults", primary_key: ["testID", "dateAndTime"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "testID", null: false
     t.timestamp "dateAndTime", null: false
     t.string "testName", limit: 50
     t.text "testResults"
-    t.index ["testID"], name: "testID"
   end
 
-  create_table "medicalhistory", primary_key: "medHisID", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "MedicalHistory", primary_key: "medHisID", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.timestamp "dateAndTime"
     t.text "medication"
     t.text "diseaseOrCondition"
   end
 
-  create_table "nurse", primary_key: "nurseID", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "Nurse", primary_key: "nurseID", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "nurseName", limit: 50
     t.string "department", limit: 50
   end
 
-  create_table "patienthistory", primary_key: ["patientID", "medHisID"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.integer "patientID", null: false
-    t.integer "medHisID", null: false
-    t.index ["medHisID"], name: "medHisID"
-  end
-
-  create_table "patientinfo", primary_key: "patientID", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "PatientInfo", primary_key: "patientID", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "currMedID"
     t.string "name", limit: 50
     t.date "birthDate"
@@ -79,25 +60,49 @@ ActiveRecord::Schema.define(version: 0) do
     t.index ["currMedID"], name: "currMedID"
   end
 
-  create_table "patienttest", primary_key: ["patientID", "testID", "dateAndTime"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "analyzeOrDiagnose", primary_key: ["doctorID", "medHisID"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "doctorID", null: false
+    t.integer "medHisID", null: false
+    t.index ["medHisID"], name: "medHisID"
+  end
+
+  create_table "getInfo", primary_key: ["patientID", "nurseID"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "patientID", null: false
+    t.integer "nurseID", null: false
+    t.index ["nurseID"], name: "nurseID"
+  end
+
+  create_table "patientHistory", primary_key: ["patientID", "medHisID"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "patientID", null: false
+    t.integer "medHisID", null: false
+    t.index ["medHisID"], name: "medHisID"
+  end
+
+  create_table "patientTest", primary_key: ["patientID", "testID", "dateAndTime"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "patientID", null: false
     t.integer "testID", null: false
     t.timestamp "dateAndTime", null: false
     t.index ["testID", "dateAndTime"], name: "testID"
   end
 
-  create_table "runtest", primary_key: ["nurseID", "testID", "dateAndTime"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "runTest", primary_key: ["nurseID", "testID", "dateAndTime"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "nurseID", null: false
     t.integer "testID", null: false
     t.timestamp "dateAndTime", null: false
     t.index ["testID", "dateAndTime"], name: "testID"
   end
 
-  add_foreign_key "analyzeordiagnose", "doctor", column: "doctorID", primary_key: "doctorid", name: "analyzeordiagnose_ibfk_1"
-  add_foreign_key "analyzeordiagnose", "medicalhistory", column: "medHisID", primary_key: "medhisid", name: "analyzeordiagnose_ibfk_2"
-  add_foreign_key "getinfo", "nurse", column: "nurseID", primary_key: "nurseid", name: "getinfo_ibfk_2"
-  add_foreign_key "getinfo", "patientinfo", column: "patientID", primary_key: "patientid", name: "getinfo_ibfk_1", on_delete: :cascade
-  add_foreign_key "patienthistory", "medicalhistory", column: "medHisID", primary_key: "medhisid", name: "patienthistory_ibfk_2"
-  add_foreign_key "patienthistory", "patientinfo", column: "patientID", primary_key: "patientid", name: "patienthistory_ibfk_1", on_delete: :cascade
-  add_foreign_key "patientinfo", "currentmedical", column: "currMedID", primary_key: "currmedid", name: "patientinfo_ibfk_1", on_delete: :cascade
+  add_foreign_key "PatientInfo", "CurrentMedical", column: "currMedID", primary_key: "currmedid", name: "patientinfo_ibfk_1", on_delete: :cascade
+  add_foreign_key "analyzeOrDiagnose", "Doctor", column: "doctorID", primary_key: "doctorid", name: "analyzeordiagnose_ibfk_1"
+  add_foreign_key "analyzeOrDiagnose", "MedicalHistory", column: "medHisID", primary_key: "medhisid", name: "analyzeordiagnose_ibfk_2"
+  add_foreign_key "getInfo", "Nurse", column: "nurseID", primary_key: "nurseid", name: "getinfo_ibfk_2"
+  add_foreign_key "getInfo", "PatientInfo", column: "patientID", primary_key: "patientid", name: "getinfo_ibfk_1", on_delete: :cascade
+  add_foreign_key "patientHistory", "MedicalHistory", column: "medHisID", primary_key: "medhisid", name: "patienthistory_ibfk_2"
+  add_foreign_key "patientHistory", "PatientInfo", column: "patientID", primary_key: "patientid", name: "patienthistory_ibfk_1", on_delete: :cascade
+  add_foreign_key "patientTest", "LabTestsResults", column: "dateAndTime", primary_key: "dateandtime", name: "patienttest_ibfk_2"
+  add_foreign_key "patientTest", "LabTestsResults", column: "testID", primary_key: "testid", name: "patienttest_ibfk_2"
+  add_foreign_key "patientTest", "PatientInfo", column: "patientID", primary_key: "patientid", name: "patienttest_ibfk_1", on_delete: :cascade
+  add_foreign_key "runTest", "LabTestsResults", column: "dateAndTime", primary_key: "dateandtime", name: "runtest_ibfk_2", on_delete: :cascade
+  add_foreign_key "runTest", "LabTestsResults", column: "testID", primary_key: "testid", name: "runtest_ibfk_2", on_delete: :cascade
+  add_foreign_key "runTest", "Nurse", column: "nurseID", primary_key: "nurseid", name: "runtest_ibfk_1"
 end
